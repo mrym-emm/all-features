6a288e808e2d5ff3f13e9b77534ffb6b1b0b0a38
mrym-emm <maryamalfaiz06@gmail.com>
2025-04-19 17:10:06 +0800

    fixed links -mm-

--- src/App.js
+++ src/App.js
@@ -10,7 +10,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Components: All on github</h1>
+        <h1>All files that was deployed on vercel:
+  <a href="https://github.com/mrym-emm/all-features" target="_blank">
+    All on GitHub
+  </a></h1>
+  <h1>Individual files:
+  <a href="https://github.com/mrym-emm/for-yiju" target="_blank">
+    All on GitHub
+  </a></h1>
       </header>
       <main>
         <h2>For User Story 8? Phishing simulator </h2>
