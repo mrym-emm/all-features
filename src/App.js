02c825c4bbdb1bcb069de25e604d79d705406b6a
mrym-emm <maryamalfaiz06@gmail.com>
2025-04-19 16:36:01 +0800

    add missing dependencies

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import YijuPhishingSimulator from './YijuPhishingSimulator';
+import YijuDataVis from './YijuDataVis'; 
 import YijuStoryGame from './YijuStoryGame';
 import YijuScrollRight from './YijuScrollRight'; ;
 
@@ -12,9 +13,15 @@ function App() {
         <h1>Components: All on github</h1>
       </header>
       <main>
-      <h2>For User Story 8? Phishing simulator </h2>
-      <h3>File are YijuPhishingSimulator.js</h3>
+        <h2>For User Story 8? Phishing simulator </h2>
+        <h3>File are YijuPhishingSimulator.js</h3>
         <YijuPhishingSimulator />
+
+        <h2>For User Story 3.1 Ransomware live data </h2>
+        <h3>File are YijuDataVis.js, css also inside at the end of code</h3>
+        <h5>So before here can add wht ransomware is in a attractive way for teens and mention tht its ASEAN datsa. doesnt have to be deep. and mention its live data from ransomware.live.</h5>
+        <h5>can changehowever uyou want</h5>
+        <YijuDataVis />
 
         <h2>For User Story 3.2 Data Storytelling via side scrolling platform game, Since I use the embed code, as I update, it shuold be live here </h2>
         <h3>Files YijuStoryGame.js</h3>
